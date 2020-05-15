58f0e615ab9c5fdc2eb780c8d670dea30b892a0d
Joakim Briceno <joakim.onnhage@gmail.com>
2020-05-15 15:18:32 +0200

    Initial commit

---
+++ src/main/java/com/joakim/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,4 @@
+package com.joakim.springsecurityjpa;
+
+public class SpringSecurityJpaApplicationTests {
+}
---
+++ src/test/java/com/joakim/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joakim.springsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
